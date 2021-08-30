344e7f7ee1c70e14293063f65f93aa5665ab5298
risako070310 <risako070310@gmail.com>
2021-08-30 05:59:36 +0000

    create normal count app

--- app.rb
+++ app.rb
@@ -8,3 +8,15 @@ before do
     Count.create(number: 0)
   end
 end
+
+get '/' do
+  @number = Count.first.number
+  erb :index
+end
+
+post '/plus' do
+  count = Count.first
+  count.number = count.number + 1
+  count.save
+  redirect "/"
+end
